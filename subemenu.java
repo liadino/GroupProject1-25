0c6d52c395dd1aff496c71a5d44208e0b58d1b67
hunter.dietzenbach <hunter.dietzenbach@MU-C1-22.mullen.pvt.k12.co.us>
2021-01-26 10:07:28 -0700

    this is a test

---
+++ subemenu.java
@@ -0,0 +1,16 @@
+
+public class subemenu {
+
+	public static void main(String[] args) {
+		
+	public void addStudent()
+		{
+			System.out.println("this will add a student");
+		}
+  
+		
+		
+		
+	}
+
+}
